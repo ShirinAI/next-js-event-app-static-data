7c7d92f2966c7ad3551ead86a7fbd33627b94411
ShirinAI <102027294+ShirinAI@users.noreply.github.com>
2022-12-02 13:45:30 +0200

    first commit v2

---
+++ pages/events/index.js
@@ -0,0 +1,25 @@
+import { getAllEvents } from '../../dummy-data'
+import EventList from '../../components/events/event-list'
+import EventsSearch from '../../components/events/events-search'
+import {useRouter} from 'next/router'
+import { Fragment } from 'react';
+
+
+function AllEventsPage(){
+  const router = useRouter();
+  const events = getAllEvents();
+
+  function findEvenetHandler(year, month){
+    const fullPath = `/events/${year}/${month}`;
+      router.push(fullPath);
+  }
+
+  return (
+  <Fragment>
+    <EventsSearch onSearch={findEvenetHandler}/>
+    <EventList items={events}/>
+    <h1>All Events</h1>
+  </Fragment>
+  )
+}
+export default AllEventsPage;
