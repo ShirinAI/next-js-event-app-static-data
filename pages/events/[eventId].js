d9a643709b615e22502b348ed4fbfe063d8cb990
ShirinAI <102027294+ShirinAI@users.noreply.github.com>
2022-12-11 19:36:46 +0200

    firebase api

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,14 +1,13 @@
-import { useRouter } from 'next/router'
+
 import { Fragment } from 'react'
-import { getEventById } from '../../dummy-data'
+import { getEventById, getAllEvents } from '../../helpers/api-util';
 import EventSummary from '../../components/event-detail/event-summary'
 import EventContent from '../../components/event-detail/event-content'
 import EventLogistics from '../../components/event-detail/event-logistics'
 
-function EventDetailPage() {
-  const router = useRouter();
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+function EventDetailPage(props) {
+
+  const event = props.selectedEvent;
 
   if(!event){
     return <p>No Event Found!</p>
@@ -17,11 +16,36 @@ function EventDetailPage() {
   return (
     <Fragment>
       <EventSummary title={event.title}/>
-      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title}/>
+      <EventLogistics 
+        date={event.date} 
+        address={event.location} 
+        image={event.image} 
+        imageAlt={event.title}/>
       <EventContent>
         {event.description}
       </EventContent>
     </Fragment>
  )
 }
+
+export async function getStaticProps(context){
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+  return{
+    props: {
+      selectedEvent: event
+    }
+  };
+}
+
+export async function getStaticPaths(){
+  const events = await getAllEvents();
+  const paths = events.map(event => ( { params: { eventId: event.id } } ));
+  return {
+    paths: paths,
+    fallback: false
+  }
+}
+
 export default EventDetailPage
